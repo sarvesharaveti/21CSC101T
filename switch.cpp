0c1b3bd4e95276c0a6e9d18cb08c0000e1e86b1b
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2023-02-08 09:35:42 +0530

    Create switch.cpp

---
+++ switch.cpp
@@ -0,0 +1,36 @@
+int day = 4;
+switch (day) {
+  case 1:
+    cout << "januay";
+    break;
+  case 2:
+    cout << "february";
+    break;
+  case 3:
+    cout << "march";
+    break;
+  case 4:
+    cout << "april";
+    break;
+  case 5:
+    cout << "may";
+    break;
+  case 6:
+    cout << "june";
+    break;
+  case 7:
+    cout << "july";
+    break;
+  case 8;
+    cout<<"august";
+    break;
+  case 9;
+    cout<<"september";
+    break;
+  case 10;
+    cout<<"october";
+    break;
+  case 11;
+    cout<<"november";
+    break;
+  case 12;
