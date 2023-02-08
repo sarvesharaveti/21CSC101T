7348035f4a05fd509f301add249e0e479556eaa6
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2023-02-08 08:50:01 +0530

    Create else if.cpp

---
+++ else if.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int time = 18;
+  if (time < 18) {
+    cout << "BONJOUR.";
+  } else {
+    cout << "BONNE SOIREE";
+  }
+  return 0;
+}
